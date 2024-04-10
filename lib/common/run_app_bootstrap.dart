8bd2eaa6cc494fd2dad12ee52c78406f7d19307d
Job Guldemeester <job@dutchcodingcompany.com>
2024-04-10 13:45:29 +0200

    :sparkles: Added bootstrap for running flutter apps

---
+++ lib/common/run_app_bootstrap.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:dcc_toolkit/logger/bolt_logger.dart';
+import 'package:flutter/widgets.dart';
+
+/// A function that handles errors.
+typedef OnError = void Function(Object error, StackTrace? stackTrace);
+
+/// Run a Flutter app with a bootstrap that catches errors.
+/// By default [onError] will use BoltLogger to log the error.
+Future<void> runAppBootstrap(Future<Widget> Function() builder, {OnError? onError}) async {
+  final errorLogger = onError ??
+      (exception, stackTrace) {
+        BoltLogger.shock([exception, stackTrace]);
+      };
+
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      FlutterError.onError = (details) => errorLogger(details.exception, details.stack);
+
+      runApp(await builder());
+    },
+    errorLogger,
+  );
+}
