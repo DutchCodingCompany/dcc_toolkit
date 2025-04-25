95a3b24147d87d915584d17b38cc2ba97a128b02
Niels Rijnberg <niels@dutchcodingcompany.com>
2025-04-25 11:43:48 +0200

    Add pagination to the toolkit

---
+++ lib/pagination/paginated_scroll_view.dart
@@ -0,0 +1,77 @@
+import 'package:dcc_toolkit/dcc_toolkit.dart';
+import 'package:flutter/material.dart';
+
+/// A widget that displays a list of items with pagination.
+///
+/// This widget is a [CustomScrollView] that displays a paginated list of items.
+/// It uses a [PaginationState] to manage the behavior of the widget.
+/// It also uses a [NotificationListener] to listen for scroll events and load more items when the user scrolls to the bottom of the list.
+class PaginatedScrollView<T> extends StatelessWidget {
+  /// Creates a new [PaginatedScrollView].
+  const PaginatedScrollView({
+    required this.state,
+    required this.itemBuilder,
+    this.onLoadMore,
+    this.topWidget,
+    this.bottomWidget,
+    super.key,
+  });
+
+  /// The state of the pagination.
+  final PaginationState<T> state;
+
+  /// The builder for the items.
+  final Widget Function(BuildContext, T) itemBuilder;
+
+  /// The function to call when the user scrolls to the bottom of the list, to load more items.
+  final void Function()? onLoadMore;
+
+  /// Optional widget to display at the top of the list.
+  final Widget? topWidget;
+
+  /// Optional widget to display at the bottom of the list.
+  final Widget? bottomWidget;
+
+  int get _itemCount => state.items.length;
+
+  T _fetchItem(int index) {
+    return state.items[index];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollNotification>(
+      onNotification:
+          onLoadMore != null
+              ? (notification) {
+                final metrics = notification.metrics;
+                if (metrics.extentAfter == metrics.minScrollExtent) {
+                  // We don't trigger loading if no next page is available or while we are already fetching more
+                  if (state.hasNextPage && !state.isLoading) {
+                    onLoadMore?.call();
+                    return true;
+                  }
+                }
+                return false;
+              }
+              : null,
+      child: CustomScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        slivers: [
+          if (topWidget != null) SliverToBoxAdapter(child: topWidget),
+          SliverList.builder(
+            itemCount: _itemCount,
+            itemBuilder: (context, index) => itemBuilder(context, _fetchItem(index)),
+          ),
+          if (state.hasNextPage)
+            const SliverToBoxAdapter(
+              child: Padding(padding: EdgeInsets.all(Sizes.m), child: Center(child: CircularProgressIndicator())),
+            ),
+          if (bottomWidget != null) SliverToBoxAdapter(child: bottomWidget),
+          //Bottom insets to be able to scroll the entire content above the FloatingActionButton
+          const SliverPadding(padding: Paddings.vertical48),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pagination/pagination_mixin.dart
@@ -0,0 +1,55 @@
+import 'package:dcc_toolkit/pagination/pagination_interface.dart';
+import 'package:dcc_toolkit/pagination/pagination_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// Mixin for pagination.
+///
+/// This mixin is used to handle pagination in a bloc.
+/// It provides a method to fetch the items for a given page and a method to initialize the pagination state.
+/// It also provides a method to load the next page.
+///
+/// Example:
+/// ```dart
+/// class MyBloc extends Bloc<MyEvent, MyState> with PaginationMixin<MyItem, MyState> {
+///   @override
+///   Future<List<MyItem>?> fetchPageItems(int page, String? searchQuery) async {
+///     // Fetch items logic
+///   }
+///
+///   @override
+///   Future<void> initializeState({String? searchQuery}) async {
+///     // Initialize state logic
+///   }
+/// 
+///   @override
+///   Future<void> loadNextPage(void Function(PaginationState<T>) emitState) async {
+///     // Load next page logic
+///   }
+/// }
+/// ```
+mixin PaginationMixin<T, S extends PaginationInterface<T>> on Cubit<S> {
+  /// Fetches the items for the given page
+  Future<List<T>?> fetchPageItems({required int page, String? searchQuery});
+
+  /// Initializes the pagination state
+  Future<void> initializeState({String? searchQuery});
+
+  /// Loads the next page
+  Future<void> loadNextPage(void Function(PaginationState<T>) emitState) async {
+    final paginationState = state.paginationState;
+    if (paginationState.currentPage < paginationState.lastPage) {
+      emitState(paginationState.copyWith(isLoading: true));
+      final nextPage = paginationState.currentPage + 1;
+      final nextItems = await fetchPageItems(page: nextPage, searchQuery: paginationState.searchQuery);
+      if (nextItems?.isNotEmpty ?? false) {
+        emitState(
+          paginationState.copyWith(
+            items: [...paginationState.items, ...nextItems!],
+            currentPage: nextPage,
+            isLoading: false,
+          ),
+        );
+      }
+    }
+  }
+}
